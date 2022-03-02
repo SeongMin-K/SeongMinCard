e9ac771b51eb80396ef37a49ebec3fc28cc625db
SeongMin-K <eo1tks3@naver.com>
2022-03-02 20:52:22 +0900

    Initial Commit

---
+++ SeongMinCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeongMinCard
+//
+//  Created by SeongMinK on 2022/03/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeongMinCard/SeongMinCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeongMinCardApp.swift
+//  SeongMinCard
+//
+//  Created by SeongMinK on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct SeongMinCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
